1de2e608fb2d474f87e2d13d8745587265ef5a92
Sunita752 <74800878+Sunita752@users.noreply.github.com>
2020-12-10 16:29:20 -0600

    Add files via upload

---
+++ Myhobby/src/com/practice/life/aboutlife.java
@@ -0,0 +1,50 @@
+package com.practice.life;
+
+public class aboutlife {
+	  public String  exercise =" Running";
+	    public String exercise1 = "Pushup";
+		public String exercise2 ="ABSworkout";
+	    public String demand = "happylife";
+	    public String want  = "betteropportunity";
+	    public int dancing = 2; 
+	   public Float salary = 6800.456f;
+	    public int time = 8;
+	    public int days = 5;
+	     
+	     public float xyz() {
+	    	 return  salary * 12;
+	     }
+	     
+	     public int workingHour() {
+	    	 return time * days; 
+	     }
+	     
+	public String hobby() {
+		return exercise  + " " +  exercise1 +  "  " + exercise2;
+	}
+		
+		public String wish() {
+			return demand + " " +  "and" +" " + want; 
+		}
+			
+		public int name() {
+			return dancing * time;
+		}
+		
+			
+		
+}
+			
+		
+		
+		
+		
+	
+		
+	
+
+	    		 
+		
+	
+
+
---
+++ Myhobby/src/com/practice/mylife/mylife1.java
@@ -0,0 +1,43 @@
+package com.practice.mylife;
+import com.practice.life.aboutlife;
+
+public class mylife1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  aboutlife sunita =  new aboutlife();
+  
+  int a = sunita.workingHour();
+   System.out.println(a);
+    
+   
+  float b = sunita.xyz();
+  System.out.println(b);
+  
+  String z = sunita.hobby();
+  System.out.println("i love "+ z);
+  
+  String love = sunita.wish();
+  System.out.println("I always feel to have" + " " +love);
+  
+  int activities = sunita.name();
+  System.out.println("I always dance " + activities + "hours in a week.");
+  
+  
+  
+  
+  // syso ctrl+space
+  //sout ctrl+space
+ System.out.print("Sunita");
+ System.out.println(" Khanal ");
+  
+  
+  
+   
+   
+   
+   
+   
+	}
+
+}
