8bfe5e07d158efbfa69f1185331685ccbf5625b4
Sunita752 <sunita7141@gmail.com>
2020-12-11 15:44:27 -0600

    hhhhhh

---
+++ classmeeting/src/classmeeting/Happy.java
@@ -0,0 +1,33 @@
+package classmeeting;
+
+public class Happy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String house = "Beautiful";
+		System.out.println("Beautiful");
+	
+
+int s = 35;
+System.out.println(s);
+ int w =18;
+ int d =50;
+ int multiply = w*d;
+ System.out.println(multiply);
+ 
+ float thenum =6.77f;
+ System.out.println(thenum);
+ 
+ 
+ String firstname = "Sunita";
+ String lastname = "Khanal";
+ System.out.println(firstname + " " + lastname);
+ int f = 16;
+ int g = 17;
+ System .out.println(f+g);
+
+
+	}
+}
+
---
+++ classmeeting/src/classmeeting/loops.java
@@ -0,0 +1,24 @@
+package classmeeting;
+
+public class loops {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a =1;
+		
+while(a<10) {
+	
+System.out.println(a++);
+
+
+}
+int i =0;
+do {
+
+	System.out.println(i);
+	i++;
+}
+while(i<5);
+	}
+
+}
